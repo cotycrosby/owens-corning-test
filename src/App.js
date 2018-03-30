cbf32666f3e025e53b314a4561f3c65556ff5d32
Coty Crosby <coty.crosby@gmail.com>
2018-03-29 20:57:04 -0400

    Successfully handled the get request

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 // File thats needed for data
 // https://mdms.owenscorning.com/api/v1/product/shingles?zip=43659
@@ -12,10 +13,46 @@ class App extends Component {
   constructor()
   {
     super();
-    
+
     this.state = {
-      data: []
+      data: {}
     }
+
+  }
+
+
+  getData()
+  {
+
+    $.ajax({
+      url: 'https://mdms.owenscorning.com/api/v1/product/shingles?zip=43659',
+      dataType:'json',
+      cache: false,
+
+      success: function(d)
+      {
+        this.setState({data: d}, function(){
+          console.log(this.state);
+        })
+
+      }.bind(this),
+
+      error: function(xhr, status, err)
+      {
+        console.log(err);
+      }
+    });
+
+  }
+
+  componentWillMount()
+  {
+    this.getData();
+  }
+
+  componentDidMount()
+  {
+    this.getData();
   }
 
   render() {
